4feac3bc2f23656d23a7d5d5a44c0b04b634a88f
Damir Cavajda <dcavajda@gmail.com>
2019-09-01 16:25:56 +0200

    lawfirm login materiliaze

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "url"=>"http://polaznik22.edunova.hr/",
+    "nazivApp" => "Lawfirm",
+    "dev"=> true,
+    "db" => [
+        "server"=>"localhost",
+        "baza"=>"lawfirm",
+        "korisnik"=>"edunova",
+        "lozinka"=>"edunova"
+    ]
+];
