d6a3813b292ae00a3e53004d4feedcc87aec3406
Damir Cavajda <dcavajda@gmail.com>
2019-09-25 15:43:18 +0200

    update table lawyer CRUD with grid

--- app/config.php
+++ app/config.php
@@ -1,21 +1,22 @@
 <?php
-
 return [
     "url"=>"http://polaznik22.edunova.hr/",
     "nazivApp" => "Lawfirm",
     "dev"=> true,
     "db" => [
 "server"=>"localhost",
-"baza"=>"lawfirm",
-"korisnik"=>"edunova",
-"lozinka"=>"edunova"
+"baza"=>"morfej_lawfirm",
+"korisnik"=>"morfej_dcavajda",
+"lozinka"=>"dcavajda"
     ]
 ];
 
+
 //"server"=>"localhost",
-//"baza"=>"morfej_lawfirm",
-//"korisnik"=>"morfej_dcavajda",
-//"lozinka"=>"dcavajda"
+//"baza"=>"lawfirm",
+//"korisnik"=>"edunova",
+//"lozinka"=>"edunova"
+
 
 
 
--- app/model/lawyer.php
+++ app/model/lawyer.php
@@ -1,6 +1,6 @@
 <?php
 
-class lawyer
+class Lawyer
 {
     public static function getlawyers()
     {
@@ -9,4 +9,62 @@ class lawyer
         $izraz->execute();
         return $izraz->fetchAll();
     }
+
+    public static function novi()
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare("
+        
+        insert into lawyer values
+        (null,:firstname,:lastname,:OIB,:IBAN)
+        
+        ");
+        $izraz->execute($_POST);
+    }
+
+
+    public static function read($id)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare("
+        
+        select * from lawyer where lawyer_id=:lawyer
+        
+        ");
+        $izraz->execute(['lawyer'=>$id]);
+        return $izraz->fetch(PDO::FETCH_ASSOC);
+
+    }
+
+    public static function promjeni($id)
+    {   
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare("
+        
+        update lawyer set
+        firstname=:firstname,
+        lastname=:lastname,
+        OIB=:OIB,
+        IBAN=:IBAN
+        where lawyer_id=:lawyer_id
+        
+        ");
+        $_POST['lawyer_id']=$id;
+        $izraz->execute($_POST);
+    }
+
+
+
+    public static function brisi($id)
+    {
+        $veza = DB::getInstance();
+        $izraz = $veza->prepare("
+        
+        delete from lawyer where lawyer_id=:lawyer_id
+        
+        ");
+        $izraz->execute(['lawyer_id'=>$id]);
+    }
+
+
 }
